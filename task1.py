3d55cdcc26d1aaf46e312da6c7631f8c7684028b
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-11 16:46:10 -0300

    Edited the first code and made it simple

--- task1.py
+++ task1.py
@@ -2,16 +2,14 @@
 
 
 
-import string
-def printing_words():
-    fin = open(r"C:\Users\josep\Desktop\lab-work\book.txt",encoding='utf-8')
-    WordList = []
-    for line in fin:
-        NoWhitespace = line.strip()
-        words = NoWhitespace.split()
-        for word1 in words:
-            word = word1.translate(string.punctuation).lower()
-            WordList.append(word)
-    print(WordList)
 
-printing_words()
+import string
+fin = open(r"C:\Users\josep\Desktop\lab-work\book.txt",encoding='utf-8')
+NewBook = []
+for line in fin:
+  line.strip()
+  words=line.split()
+  for word1 in words:
+      word = word1.translate(string.punctuation).lower()
+      NewBook.append(word)
+print(NewBook)
