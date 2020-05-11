314105e7c273c4d7fc672afa72d62298c74f1ba3
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-11 15:48:36 -0300

    Added first code

--- task1.py
+++ task1.py
@@ -1,6 +1,12 @@
 # This is the first code
-
+import string
+print(string.punctuation)
 f_input=open("books.txt")
+Lst1=[]
 for line in f_input():
-    word=line.strip()
-    print(word)
+    word=NoWhitespace.split()
+    for word1 in word:
+      word=word1.translate(string.punctuation).lower()
+      Lst1=Lst1+[word]
+    print(Lst1)
+
