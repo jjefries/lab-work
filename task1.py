34255f357c13d1480156cf1b0d5ae9bbab45e3a0
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-11 16:13:45 -0300

    Done and executed task 1 from lab 4

--- task1.py
+++ task1.py
@@ -1,12 +1,17 @@
 # This is the first code
-import string
-print(string.punctuation)
-f_input=open("books.txt")
-Lst1=[]
-for line in f_input():
-    word=NoWhitespace.split()
-    for word1 in word:
-      word=word1.translate(string.punctuation).lower()
-      Lst1=Lst1+[word]
-    print(Lst1)
 
+
+
+import string
+def printing_words():
+    fin = open(r"C:\Users\josep\Desktop\lab-work\book.txt",encoding='utf-8')
+    WordList = []
+    for line in fin:
+        NoWhitespace = line.strip()
+        words = NoWhitespace.split()
+        for word1 in words:
+            word = word1.translate(string.punctuation).lower()
+            WordList.append(word)
+    print(WordList)
+
+printing_words()
