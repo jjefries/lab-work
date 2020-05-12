972c2f360b2513092e82088a7031c0795f4efd71
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-12 13:48:37 -0300

    Added second method for task 1

--- task1.py
+++ task1.py
@@ -13,3 +13,16 @@ for line in fin:
       word = word1.translate(string.punctuation).lower()
       NewBook.append(word)
 print(NewBook)
+
+
+# Second method
+
+
+import string
+file_name=open(r"C:\Users\josep\Desktop\lab-work\book.txt", encoding="utf-8")
+new_book=[]
+for line in file_name:
+  for word in line.split():
+    new_word=word.strip(string.punctuation).lower()
+    new_book.append(new_word)
+print(new_book)
