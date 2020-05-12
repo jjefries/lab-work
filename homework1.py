88f8087910e84eeaa27c927faeb0b13c6fa37997
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-12 19:31:24 -0300

    created the file

---
+++ homework1.py
@@ -0,0 +1,2 @@
+# Modify the previous program to read a word list and then print all the words in the book that are not in the word list. How many of them are typos? How many of them are common words that should be in the word list, and how many of them are obscure?
+# Python provides a data structure called set that provides many common set operations. Read the documentation and write a program that uses set subtraction to find words in the book that are not in the word list.
