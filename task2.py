c90dd924d361e49294917678c0733c48fad6c629
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-12 16:27:40 -0300

    Updated second code

--- task2.py
+++ task2.py
@@ -1,24 +1,40 @@
 # This is the second code
 
 import string
-fin = open(r"C:\Users\josep\Desktop\lab-work\book.txt",encoding='utf-8')
-NewBook = []
+file_name=open(r"C:\Users\josep\Desktop\lab-work\book.txt", encoding="utf-8")
+new_book=[]
 D={}
+first_twenty_keys=[]
+first_twenty=[]
+different_words=[]
 count=0
-for line in fin:
-  line.strip()
-  words=line.split()
-  for word1 in words:
-      word = word1.translate(string.punctuation).lower()
-      NewBook.append(word)
+for line in file_name:
+  for word in line.split():
+    new_word=word.strip(string.punctuation).lower()
+    new_book.append(new_word)
+    if new_word not in new_book:
+      count=1
+      D.update({word:count})
+    else:
+      count=count+1
+      D.update({word:count})
 
-      if word not in NewBook:
-          count=1
-          D.update({word: count})
-      else:
-          count+=1
-          D.update({word: count})
-print(NewBook)
-print(len(NewBook))
-print(D)
-print(sorted(D.values()))
+print(new_book)
+print("\n")
+print("The total number of words in the book are: ", len(new_book))
+print("\n")
+print("Number of times each word is used: ", D)
+print("\n")
+
+for keys in sorted(D.values(),reverse=True)[:20]:
+  first_twenty_keys.append(keys)
+for i in first_twenty_keys:
+  for val,key in D.items():
+    if i ==key:
+      first_twenty.append(val)
+print("The first 20 frequently used words are:",first_twenty)
+
+for key,val in D.items():
+  if val==1:
+    different_words.append(key)
+print("Different words used in the book are:", different_words)
