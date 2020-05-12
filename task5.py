11c80fbff56ee4c50513114501186938b750c8a6
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-12 19:10:34 -0300

    Added task 5 code

--- task5.py
+++ task5.py
@@ -1,1 +1,15 @@
 # This is fifth task
+
+def sed(file_name_1, file_name_2):
+  with open(file_name_1, mode='r') as f1:
+    with open(file_name_2, mode='w') as f2:
+      for line in f1:
+        f1.readline()
+        for line_1 in f2:
+          f2.write(line_1)
+try:
+  print("Successfully copied the contents from one file to another")
+except FileNotFoundError:
+  print("File is not found. Therefore cannot copy the contents or read the contents")
+
+sed(r"/content/text1.txt", r"/content/text.txt")
