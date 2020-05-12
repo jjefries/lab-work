a7deed2eb829a5391347a37b1d6ee25ab557e033
jjefries <64707905+jjefries@users.noreply.github.com>
2020-05-12 18:47:37 -0300

    updated third code

--- task4.py
+++ task4.py
@@ -1,1 +1,11 @@
 # This is task 4
+
+import os
+def walk(dirname):
+  for name in os.listdir(dirname):
+    path=os.path.join(dirname, name)
+    if os.path.isfile(path):
+      print(path)
+    else:
+      walk(path)
+walk("C:\Users\josep\Desktop\Python\Lab")
